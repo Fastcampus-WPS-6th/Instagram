60d310dfbc3f2db92184a9305999f6f276dabcaf
LeeHanYeong <dev@azelf.com>
2017-10-13 16:00:59 +0900

    settings의 DATABASES설정을 config_secret에서 가져오도록 설정

--- instagram/config/settings.py
+++ instagram/config/settings.py
@@ -93,13 +93,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+# 여기 채워주세요
+DATABASES = config_secret_common['django']['databases']
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
