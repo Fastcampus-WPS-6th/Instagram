d23dabc281d6235c40044b1e7f362970ee3ff3f2
LeeHanYeong <dev@azelf.com>
2017-10-17 12:29:45 +0900

    username field custom validation

--- instagram/member/forms.py
+++ instagram/member/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class SignupForm(forms.Form):
@@ -10,9 +13,19 @@ class SignupForm(forms.Form):
         )
     )
     password = forms.CharField(
+        # required=False,
         widget=forms.PasswordInput(
             attrs={
                 'class': 'form-control'
             }
         )
     )
+
+    # clean_<field_name>
+    def clean_username(self):
+        data = self.cleaned_data['username']
+        # 유저가 존재하면 forms.ValidationError를 발생시킴
+        # 아니면 data를 리턴
+        if User.objects.filter(username=data).exists():
+            raise forms.ValidationError('오아ㅏ아아아아아')
+        return data
--- instagram/member/views.py
+++ instagram/member/views.py
@@ -16,19 +16,18 @@ def signup(request):
             # 통과한 경우 정제된 데이터 (cleaned_data)에서 username과 password항목을 가져옴
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
-
-            # 이미 username항목이 주어진 username값으로 존재하는 User가 있는지 검사
-            if User.objects.filter(username=username).exists():
-                return HttpResponse(f'Username {username} is already exist')
             # username, password가 주어졌고 중복되는 User가 없다면 User생성
             user = User.objects.create_user(
                 username=username,
                 password=password
             )
             return HttpResponse(f'{user.username}, {user.password}')
+        print(form.cleaned_data)
+        print(form.errors)
 
     # GET요청시 SignupForm인스턴스를 form변수에 할당, context에 같은 키/값으로 전달
-    form = SignupForm()
+    else:
+        form = SignupForm()
     context = {
         'form': form,
     }
