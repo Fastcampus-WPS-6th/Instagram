fb8604149a0fce172a51112799edf2fd30bf675c
LeeHanYeong <dev@azelf.com>
2017-10-17 11:42:02 +0900

    SignupForm을 사용

--- instagram/member/views.py
+++ instagram/member/views.py
@@ -2,15 +2,21 @@ from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import SignupForm
+
 User = get_user_model()
 
 
 def signup(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+        # 데이터가 binding된 SignupForm인스턴스를 생성
+        form = SignupForm(request.POST)
+        # 해당 form이 자신의 필드에 유효한 데이터를 가지고 있는지 유효성 검사
+        if form.is_valid():
+            # 통과한 경우 정제된 데이터 (cleaned_data)에서 username과 password항목을 가져옴
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
 
-        if username and password:
             # 이미 username항목이 주어진 username값으로 존재하는 User가 있는지 검사
             if User.objects.filter(username=username).exists():
                 return HttpResponse(f'Username {username} is already exist')
@@ -21,4 +27,9 @@ def signup(request):
             )
             return HttpResponse(f'{user.username}, {user.password}')
 
-    return render(request, 'member/signup.html')
+    # GET요청시 SignupForm인스턴스를 form변수에 할당, context에 같은 키/값으로 전달
+    form = SignupForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'member/signup.html', context)
