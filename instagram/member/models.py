00be084efe9d6827911d3997a56f81c80b9c737d
LeeHanYeong <dev@azelf.com>
2017-10-23 16:24:22 +0900

    follow_toggle기능 구현

--- instagram/member/models.py
+++ instagram/member/models.py
@@ -43,6 +43,34 @@ class User(AbstractUser):
         verbose_name = '사용자'
         verbose_name_plural = f'{verbose_name} 목록'
 
+    def follow_toggle(self, user):
+        # 1. 주어진 user가 User객체인지 확인
+        #    아니면 raise ValueError()
+        # 2. 주어진 user를 follow하고 있으면 해제
+        #    안 하고 있으면 follow함
+        if not isinstance(user, User):
+            raise ValueError('"user" argument must be User instance!')
+
+        relation, relation_created = self.following_user_relations.get_or_create(to_user=user)
+        if relation_created:
+            return True
+        relation.delete()
+        return False
+
+        # if user in self.following_users.all():
+        #     Relation.objects.filter(
+        #         from_user=self,
+        #         to_user=user,
+        #     ).delete()
+        # else:
+        #     # Relation중개모델을 직접 사용하는 방법
+        #     Relation.objects.create(
+        #         from_user=self,
+        #         to_user=user,
+        #     )
+        #     # Relation에 대한역참조 매니저를 사용하는 방법
+        #     self.following_user_relations.create(to_user=user)
+
 
 class Relation(models.Model):
     # User의 follow목록을 가질 수 있도록
