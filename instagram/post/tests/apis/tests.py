2a77fda0ded2b48e86b19ae0bc1f384b6af69e14
LeeHanYeong <dev@azelf.com>
2017-11-09 15:33:07 +0900

    post_list get요청에 대한 테스트코드 작성

--- instagram/post/tests/apis/tests.py
+++ instagram/post/tests/apis/tests.py
@@ -1,7 +1,16 @@
+import io
+from random import randint
+
+from django.contrib.auth import get_user_model
+from django.core.files import File
 from django.urls import reverse, resolve
+from rest_framework import status
 from rest_framework.test import APILiveServerTestCase
 
 from post.apis import PostList
+from post.models import Post
+
+User = get_user_model()
 
 
 class PostListViewTest(APILiveServerTestCase):
@@ -24,3 +33,26 @@ class PostListViewTest(APILiveServerTestCase):
         self.assertEqual(
             resolver_match.func.view_class,
             self.VIEW_CLASS)
+
+    def test_get_post_list(self):
+        user = User.objects.create_user(username='dummy', age=0)
+        # 0이상 20이하의 임의의 숫자 지정
+        num = randint(0, 20)
+        # num개수만큼 Post생성, author를 지정해줌
+        for i in range(num):
+            Post.objects.create(
+                author=user,
+                photo=File(io.BytesIO()),
+            )
+
+        url = reverse(self.URL_API_POST_LIST_NAME)
+        # post_list에 GET요청
+        response = self.client.get(url)
+        # status code가 200인지 확인
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # objects.count결과가 num과 같은지 확인
+        self.assertEqual(Post.objects.count(), num)
+        # response로 돌아온 JSON리스트의 길이가 num과 같은지 확인
+        self.assertEqual(len(response.data), num)
+
+        
