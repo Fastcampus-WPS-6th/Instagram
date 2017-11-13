f196fd3f3b73c0ca570d2b56f44b7e0c9b2d1e7b
LeeHanYeong <dev@azelf.com>
2017-11-13 16:53:06 +0900

    FacebookLoginAPI의 debug_token구현

--- instagram/member/apis.py
+++ instagram/member/apis.py
@@ -1,6 +1,11 @@
+from typing import NamedTuple
+
+import requests
+from django.conf import settings
 from django.contrib.auth import authenticate, get_user_model
 from rest_framework import status
 from rest_framework.authtoken.models import Token
+from rest_framework.exceptions import APIException
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -43,4 +48,41 @@ class Signup(APIView):
 class FacebookLogin(APIView):
     # /api/member/facebook-login/
     def post(self, request):
-        print(request.data)
+        # request.data에
+        #   access_token
+        #   facebook_user_id
+        #       데이터가 전달됨
+
+        # Debug결과의 NamedTuple
+        class DebugTokenInfo(NamedTuple):
+            app_id: str
+            application: str
+            expires_at: int
+            is_valid: bool
+            issued_at: int
+            scopes: list
+            type: str
+            user_id: str
+
+        # token(access_token)을 받아 해당 토큰을 Debug
+        def get_debug_token_info(token):
+            app_id = settings.FACEBOOK_APP_ID
+            app_secret_code = settings.FACEBOOK_APP_SECRET_CODE
+            app_access_token = f'{app_id}|{app_secret_code}'
+
+            url_debug_token = 'https://graph.facebook.com/debug_token'
+            params_debug_token = {
+                'input_token': token,
+                'access_token': app_access_token,
+            }
+            response = requests.get(url_debug_token, params_debug_token)
+            return DebugTokenInfo(**response.json()['data'])
+
+        # request.data로 전달된 access_token값을 페이스북API쪽에 debug요청, 결과를 받아옴
+        debug_token_info = get_debug_token_info(request.data['access_token'])
+
+        if debug_token_info.user_id != request.data['facebook_user_id']:
+            raise APIException('페이스북 토큰의 사용자와 전달받은 facebook_user_id가 일치하지 않음')
+
+        if not debug_token_info.is_valid:
+            raise APIException('페이스북 토큰이 유효하지 않음')
