7846961e10e12facd47e091d4bbfb68a54c2a864
LeeHanYeong <dev@azelf.com>
2017-11-13 12:12:48 +0900

    Login에서 user정보 리턴

--- instagram/member/apis.py
+++ instagram/member/apis.py
@@ -7,34 +7,26 @@ from rest_framework.views import APIView
 
 class Login(APIView):
     def post(self, request, *args, **kwargs):
-        print(request.data)
         username = request.data['username']
         password = request.data['password']
-
-        # 전달받은 username, password값으로
-        # authenticate실행
         user = authenticate(
             username=username,
             password=password,
         )
-        # user가 존재할 경우 (authenticate에 성공)
         if user:
+            # 'user'키에 다른 dict로 유저에 대한 모든 정보를 보내줌
             token, token_created = Token.objects.get_or_create(user=user)
             data = {
                 'token': token.key,
-                'username': user.username,
+                'user': {
+                    'pk': user.pk,
+                    'username': user.username,
+                    'img_profile': user.img_profile.url if user.img_profile else '',
+                    'age': user.age,
+                }
             }
             return Response(data, status=status.HTTP_200_OK)
-        # 인증에 실패한 경우
-        else:
-            data = {
-                'username': username,
-                'password': password,
-            }
-            return Response(data, status=status.HTTP_401_UNAUTHORIZED)
-        # 전달받은 데이터: request.data를 사용
-        # URL: /api/member/login/
-        # 1. username/password를 받음
-        # 2. authenticate를 이용해 사용자 인증
-        # 3. 인증된 사용자에 해당하는 토큰을 생성
-        # 4. 사용자 정보와 token.key값을 Response로 돌려줌
+        data = {
+            'message': 'Invalid credentials'
+        }
+        return Response(data, status=status.HTTP_401_UNAUTHORIZED)
